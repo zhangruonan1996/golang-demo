4f2cc976af46ea9716315227c6f55fd296a0e9c3
dengenxi <coderqin@foxmail.com>
2025-03-26 12:00:08 +0800

    返回一个随机的问候语

--- 02-创建模块/greetings/greetins.go
+++ 02-创建模块/greetings/greetins.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello 为指定的人返回问候语.
@@ -14,6 +16,26 @@ func Hello(name string) (string, error) {
 
 	// 如果接收到名称，则返回一个嵌入名称的值
 	// 在问候消息中.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// init 为函数中使用的变量设置初始值.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat 返回一组问候消息中的一个。返回的
+// 消息是随机选择的.
+func randomFormat() string {
+	// 消息格式的切片.
+	formats := []string{
+		"Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+	}
+
+	// 通过为格式切片指定随机索引
+    // 来返回随机选择的消息格式.
+	return formats[rand.Intn(len(formats))]
 }
