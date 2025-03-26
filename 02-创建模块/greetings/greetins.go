3d4f54b1317281efc7c42a720bb499c3ddc46341
dengenxi <coderqin@foxmail.com>
2025-03-26 11:55:19 +0800

    如果名称为空，则向调用方返回错误

--- 02-创建模块/greetings/greetins.go
+++ 02-创建模块/greetings/greetins.go
@@ -1,10 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello 为指定的人返回问候语.
-func Hello(name string) string {
-	// 返回在消息中嵌入名称的问候语.
+func Hello(name string) (string, error) {
+	// 如果没有给出名字，返回一个带有消息的错误.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// 如果接收到名称，则返回一个嵌入名称的值
+	// 在问候消息中.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
