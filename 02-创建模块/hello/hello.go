b2456bd25717a802783c4226083a599ed762e0db
dengenxi <coderqin@foxmail.com>
2025-03-26 11:56:39 +0800

    处理错误

--- 02-创建模块/hello/hello.go
+++ 02-创建模块/hello/hello.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"zhangruonan.org/greetings"
 )
 
 func main() {
-	// 获取问候信息并打印出来.
-	message := greetings.Hello("章若楠")
+	// 设置预定义Logger的属性，包括
+    // 日志条目前缀和禁用打印的标志
+    //  时间、源文件和行号.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// 请求问候消息.
+	message, err := greetings.Hello("")
+	// 如果返回错误，则将其打印到控制台并
+    // 退出程序.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	// 如果没有返回错误，则打印返回的消息
+    // 到控制台.
 	fmt.Println(message)
 }
